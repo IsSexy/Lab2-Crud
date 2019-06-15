0a4eb1c39e1af4d8477103bfc9e81425b30e45c5
Brady W <i.am.brady.ward@gmail.com>
2019-06-15 15:37:56 -0400

    Quick push

---
+++ app.js
@@ -0,0 +1,7 @@
+//Our dotenv environment
+require('dotenv').config();
+
+//Imported libraries
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
