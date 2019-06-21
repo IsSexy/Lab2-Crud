2212af31d1503078eae7b032858092002a642612
Brady W <i.am.brady.ward@gmail.com>
2019-06-21 11:12:10 -0400

    Finished

--- app.js
+++ app.js
@@ -6,8 +6,42 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 
-//Converting express to an app constant
+//Assigning express to an app constant
 const app = express();
+
+//Connect to db
+mongoose.connect(process.env.DB_URI, {
+    auth: {
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD
+    },
+    useNewUrlParser: true
+}).catch(err => console.log(`ERROR: ${err}`));
+
+//Flash support
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+app.use(session({
+    secret: (process.env.secret || 'i-am-a-salty-boy'),
+    cookie: {
+        max: 10800000
+    },
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.flash = res.locals.flash || {};
+    res.locals.flash.success = req.flash('success') || null;
+    res.locals.flash.error = req.flash('error') || null;
+    
+    next();
+});
 
 //Body parser
 const bodyParser = require('body-parser');
@@ -15,6 +49,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+//Assets
+app.use('/css', express.static('./Assets/stylesheets'));
 
 //Our views path
 app.set('Views', path.join(__dirname, 'Views'));
